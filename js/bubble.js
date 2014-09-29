c98aa05c427d5a73a114d0fb207cf6684c9728e6
oldhill <oldhillcode@gmail.com>
2014-09-28 21:23:47 -0700

    Add bubblesort in js

---
+++ js/bubble.js
@@ -0,0 +1,33 @@
+/**
+ * Bubble sort with timer
+ */
+var bubbleSort = function(input) {
+  var startTimer = new Date();
+  var sorted = 'false';
+  var swapper = 0;
+
+  while (sorted === 'false') {
+    sorted = 'true'; 
+    for (var i = 0; i < input.length; i++) {
+      if (input[i] > input[i + 1]) {
+        swapper = input[i];
+        input[i] = input[i + 1];
+        input[i + 1] = swapper;
+        sorted = 'false';
+      }
+    }      
+  }
+  var stopTimer = new Date();
+  var results = {
+    elapsedTime: stopTimer - startTimer,
+    sorted: input
+  }
+  return results;
+}
+
+var my_array = [2, 3, 4, 3, -1, 2, 1, 8, 0, 5, 3, 5, 3, 5, 3,2, 5, 99];
+console.log('Unsorted: ' + my_array);
+
+var results = bubbleSort(my_array);
+console.log('Sorted: ' + results.sorted);
+console.log('In: ' + results.elapsedTime + ' milliseconds.');
