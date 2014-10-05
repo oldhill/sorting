34aa5a562dc886f74c2d02b3d3de974bfe504c3f
oldhill <oldhillcode@gmail.com>
2014-10-05 11:46:56 -0700

    Add some comments

--- js/merge.js
+++ js/merge.js
@@ -4,6 +4,7 @@
  * http://en.wikipedia.org/wiki/Merge_sort
  */
 
+
  /**
   * Helper function. Merges two sorted arrays into a single sorted array.
   */
@@ -27,26 +28,25 @@ var merge = function(left, right) {
 };
 
  /**
-  * Process controller. Breaks arrays down recursively, merges back together.
+  * Process controller. Splits array into single element arrays, then merges them back together into one sorted array.
   */
 var mergeSort = function(input) {
-  console.log('mergSort input: ' + input);
-  // Base case
-  if (input.length <= 1) {
+  // Base case. Single element array is always sorted.
+  if (input.length < 2) {
     return input;
   }
 
-  // Divide
+  // Divide array into ~halves
   var medianIndex = Math.floor(input.length / 2);
   var left = input.slice(0, medianIndex);
   var right = input.slice(medianIndex, input.length);
 
   // Recur (sort sublists)
-  left = mergeSort(left);
-  right = mergeSort(right);
+  var sorted_left = mergeSort(left);
+  var sorted_right = mergeSort(right);
 
   // Merge
-  return merge(left, right);
+  return merge(sorted_left, sorted_right);
 };
 
 var my_array = [2, 3, 4, 3, -1, 2, 1, 8, 0, 5, 3, 5, 3, 5, 3,2, 5, 99];
